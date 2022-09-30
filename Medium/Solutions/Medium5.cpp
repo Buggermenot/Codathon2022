af09c2b9a72e04229ae5e405344e4a1913d2f09c
Tanmay Verma <94090874+Buggermenot@users.noreply.github.com>
2022-09-30 22:40:31 +0530

    Update Medium5.cpp

---
+++ Medium/Solutions/Medium5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int main(){
+    int w, n;
+    cin >> w >> n;
+    queue<int> arr;
+    int a;
+    cin >> a;
+    arr.push(a);
+    int wait[];
+    for (int i = 1; i < w; i++){
+        cin >> a;
+        if (a > queue.back())
+    }
+}
